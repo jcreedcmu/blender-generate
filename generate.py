0cc2c60e2a574c2fe2e1abf7a6725018d17643cb
Jason Reed <jreed@gmail.com>
2024-02-21 13:45:43 -0500

    Displacement working correctly

--- generate.py
+++ generate.py
@@ -35,23 +35,30 @@ def tileMat():
     for node in nodes:
         nodes.remove(node)
 
-    # principled bsdf node
-    principled_bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
-    principled_bsdf.inputs['Base Color'].default_value =( 0.95, 0.829, 0.05, 1)
-    principled_bsdf.inputs['Roughness'].default_value = 0.5
-    principled_bsdf.inputs['Metallic'].default_value = 0.1
-
     # texture node
     texImage = nodes.new(type='ShaderNodeTexImage')
     texImage.extension = 'EXTEND'
     bpy.ops.image.open(filepath="/home/jcreed/proj/blender-generate/W.png")
     texImage.image = bpy.data.images.get("W.png")
 
+    # principled bsdf node
+    principled_bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
+    principled_bsdf.inputs['Base Color'].default_value =( 0.95, 0.829, 0.05, 1)
+    principled_bsdf.inputs['Roughness'].default_value = 0.5
+    principled_bsdf.inputs['Metallic'].default_value = 0.1
+    # material.node_tree.links.new(texImage.outputs['Color'], principled_bsdf.inputs['Base Color']);
+
+    # displacement node
+    displacement = nodes.new(type='ShaderNodeDisplacement')
+    displacement.space = 'OBJECT'
+    displacement.inputs['Scale'].default_value = -0.05
+    material.node_tree.links.new(texImage.outputs['Color'], displacement.inputs['Height']);
+
     # output node
     output_node = nodes.new(type='ShaderNodeOutputMaterial')
     material.node_tree.links.new(principled_bsdf.outputs['BSDF'], output_node.inputs['Surface'])
+    material.node_tree.links.new(displacement.outputs['Displacement'], output_node.inputs['Displacement'])
 
-    material.node_tree.links.new(texImage.outputs['Color'], principled_bsdf.inputs['Base Color']);
     return material
 
 mat = tileMat()
@@ -64,12 +71,18 @@ def tile():
   plane.select_set(True)
   bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
 
-  # mod = plane.modifiers.new(name="Subdivision", type='SUBSURF')
+  bpy.context.scene.cycles.feature_set = 'EXPERIMENTAL'
+
+  mod = plane.modifiers.new(name="Subdivision", type='SUBSURF')
+  mod.subdivision_type = 'SIMPLE'
+  bpy.context.object.cycles.use_adaptive_subdivision = True
+  mat.cycles.displacement_method = 'DISPLACEMENT'
+
   # mod.render_levels = 3  # The number of subdivisions during render time
 
-  mod = plane.modifiers.new(name="Bevel", type='BEVEL')
-  mod.width = 0.07
-  mod.segments = 3
+  # mod = plane.modifiers.new(name="Bevel", type='BEVEL')
+  # mod.width = 0.07
+  # mod.segments = 3
 
   plane.data.materials.clear()
   plane.data.materials.append(mat)
